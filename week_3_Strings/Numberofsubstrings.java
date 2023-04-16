2e02b667e4c60c21e5d56f5ef49d250f8c930ab5
Akash Narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-04-16 09:30:26 +0530

    Week 4 -Day 3

---
+++ week_3_Strings/Numberofsubstrings.java
@@ -0,0 +1,31 @@
+package week_3_Strings;
+
+//Number of substrings of one string present in other
+public class Numberofsubstrings {
+
+	public static void main(String[] args) {
+		String s1 = "aab",s2 = "aaaab";
+		System.out.println(countSubstrs(s1, s2));
+	}
+	
+	static int countSubstrs(String s1,String s2)
+		{
+		int ans = 0;
+		
+		for (int i = 0; i < s1.length(); i++)
+		{
+	
+		String s3 = "";
+		char[] s4 = s1.toCharArray();
+		for (int j = i; j < s1.length(); j++)
+		{
+		s3 += s4[j];
+		
+		if (s2.indexOf(s3) != -1)
+		ans++;
+		}
+		}
+		return ans;
+		}
+
+}
