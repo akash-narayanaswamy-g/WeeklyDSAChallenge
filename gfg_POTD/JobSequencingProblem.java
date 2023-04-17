ec686266edfb9becf36afc31faaa0b5392d33ae0
Akash Narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-04-17 21:28:17 +0530

    Day 19

---
+++ gfg_POTD/JobSequencingProblem.java
@@ -0,0 +1,36 @@
+package gfg_POTD;
+
+import java.util.*;
+
+//Job Sequencing Problem
+
+class Job{
+	int id,profit,deadline;
+}
+public class JobSequencingProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	int[] JobScheduling(Job arr[], int n)
+    {
+        boolean[] jobsDone = new boolean[n];
+        Arrays.fill(jobsDone, false);
+        Arrays.sort(arr, (a, b) -> b.profit - a.profit);
+        int cnt = 0, profit = 0;
+        for (Job job : arr) {
+            for (int i = Math.min(job.deadline - 1, n - 1); i >= 0; i--) {
+                if (!jobsDone[i]) {
+                    cnt++;
+                    profit += job.profit;
+                    jobsDone[i] = true;
+                    break;
+                }
+            }
+        }
+        return new int[]{cnt, profit};
+    }
+
+}
