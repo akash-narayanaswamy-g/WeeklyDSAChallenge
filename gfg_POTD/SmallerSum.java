3513154feb2835c21a616c01544634698e23be51
Akash Narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-04-22 20:25:15 +0530

    Day 24

---
+++ gfg_POTD/SmallerSum.java
@@ -0,0 +1,52 @@
+package gfg_POTD;
+
+import java.util.*;
+
+//Smaller Sum
+public class SmallerSum {
+
+	public static void main(String[] args) {
+		int n = 3,arr[] = {1, 2, 3};
+		System.out.println(Arrays.toString(smallerSum(n, arr)));
+
+	}
+	
+	public static long[] smallerSum(int n,int arr[])
+    {
+		HashMap<Integer,Long> map=new HashMap<>();
+        long[] ans=new long[n]; int in=0;
+        int[] arrcpy=new int[n];
+        
+        for(int i=0;i<n;i++) arrcpy[i]=arr[i]; 
+        
+        
+        Arrays.sort(arrcpy);
+        
+        long prefixSum=arrcpy[0];
+        map.put(arrcpy[0],(long)0);
+        
+        for(int i=1;i<n;i++)
+        {
+            if(arrcpy[i-1]==arrcpy[i])
+            {
+                long sum=map.get(arrcpy[i-1]);
+                map.put(arrcpy[i],sum);
+            }
+            else
+            {
+                map.put(arrcpy[i],prefixSum);
+            }
+            
+            prefixSum+=arrcpy[i];
+        }
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=map.get(arr[i]);
+        }
+        
+            return ans;
+    }
+
+}
