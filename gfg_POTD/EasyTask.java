862effdd32b1baa392c1df7a093dcc23aca46534
Akash Narayanaswamy <akashnarayanaswamy.ng@gmail.com>
2023-04-27 14:35:15 +0530

    Day 29

---
+++ gfg_POTD/EasyTask.java
@@ -0,0 +1,62 @@
+package gfg_POTD;
+import java.util.*;
+//Easy Task
+public class EasyTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static ArrayList<Character> easyTask(int n,String s,int q,query queries[])
+    {
+        char arr[] = s.toCharArray();
+        ArrayList<Character> ans = new ArrayList<>();
+        for(int i=0;i<q;i++){
+            if(queries[i].type.equals("1")){
+                arr[Integer.valueOf(queries[i].a)] = queries[i].b.charAt(0);
+            }
+            else if (queries[i].type.equals("2")){
+                int freq[] = new int[26];
+                int a = Integer.valueOf(queries[i].a);
+                int b = Integer.valueOf(queries[i].b);
+                int c = Integer.valueOf(queries[i].c);
+                
+                for(int j=a;j<=b;j++){
+                    freq[arr[j]-97]++;
+                }
+                int count=0;
+
+                for(int j=25;j>=0;j--){
+                    if(freq[j]!=0){
+                        
+                        count = count + freq[j];
+                        
+                        if(count>=c){
+                            ans.add((char)(j+97));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+
+    }
+
+}
+
+class query
+{
+    String type;
+    String a;
+    String b;
+    String c;
+    public query(String type,String a,String b,String c)
+    {
+        this.type=type;
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+}
